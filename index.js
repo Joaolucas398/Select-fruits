2b061134533a1b3e52a0cfc044a48b61440bee90
João <joaolucasdesouzapaz1@gmail.com>
2023-07-10 21:09:46 -0300

    implemented remove fruit button

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function preencherDropdown() {
     "Morango",
     "Melancia",
   ];
+
+  //criando o Dropdown com o array de frutas
   let select = document.getElementById("frutas");
   for (var i = 0; i < frutas.length; i++) {
     let option = document.createElement("option");
@@ -17,25 +19,33 @@ function preencherDropdown() {
     select.appendChild(option);
   }
 
+  //Criando e exibindo a lista de frutas selecinadas
   const frutasSelecionadas = [];
   var ulElement = document.getElementById("frutasSelecionadas");
 
   select.addEventListener("change", function () {
     const opcoesSelecionadas = select.selectedOptions;
-
     ulElement.innerHTML = "";
-    // Itera sobre as opções selecionadas e adiciona ao array
+
     for (var i = 0; i < opcoesSelecionadas.length; i++) {
       const frutaSelecionada = opcoesSelecionadas[i].value;
       frutasSelecionadas.push(frutaSelecionada);
     }
 
-    console.log(frutasSelecionadas);
-
     for (var i = 0; i < frutasSelecionadas.length; i++) {
+      const index = i;
       let cardFrutas = document.createElement("li");
       cardFrutas.textContent = frutasSelecionadas[i];
+
+      let removeButton = document.createElement("button");
+      removeButton.textContent = "remover";
+      removeButton.addEventListener("click", function () {
+        frutasSelecionadas.splice(index, 1);
+        cardFrutas.remove();
+      });
+
       ulElement.appendChild(cardFrutas);
+      cardFrutas.appendChild(removeButton);
     }
   });
 }
