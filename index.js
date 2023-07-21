37a54223e519b1aae70c98fbb6b770defb5186d3
Joao <Joao.Paz@AB-inbev.com>
2023-07-21 16:30:01 -0300

    adjust css

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ function preencherDropdown() {
 
   //criando o Dropdown com o array de frutas
   let select = document.getElementById("frutas");
-  for (var i = 0; i < frutas.length; i++) {
+  for (const element of frutas) {
     let option = document.createElement("option");
-    option.value = frutas[i];
-    option.text = frutas[i];
+    option.value = element;
+    option.text = element;
     select.appendChild(option);
   }
 
   //Criando e exibindo a lista de frutas selecinadas
   let frutasSelecionadas = [];
-  var ulElement = document.getElementById("frutasSelecionadas");
+  let ulElement = document.getElementById("frutasSelecionadas");
 
   select.addEventListener("change", function () {
     let opcoesSelecionadas = select.selectedOptions;
     ulElement.innerHTML = "";
 
-    for (var i = 0; i < opcoesSelecionadas.length; i++) {
-      let frutaSelecionada = opcoesSelecionadas[i].value;
+    for (const element of opcoesSelecionadas) {
+      let frutaSelecionada = element.value;
       frutasSelecionadas.push(frutaSelecionada);
     }
 
-    for (var i = 0; i < frutasSelecionadas.length; i++) {
+    for (let i = 0; i < frutasSelecionadas.length; i++) {
       let index = i;
       let cardFrutas = document.createElement("li");
       cardFrutas.textContent = frutasSelecionadas[i];
 
       let removeButton = document.createElement("button");
-      removeButton.textContent = "remover";
+      removeButton.textContent = "X";
       removeButton.addEventListener("click", function () {
         frutasSelecionadas.splice(index, 1);
         cardFrutas.remove();
